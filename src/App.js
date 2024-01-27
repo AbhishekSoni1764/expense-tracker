decb88c8ab2e7cd05a5124dfc4d3a9a783bc9e28
Abhishek <abhisoni1764@gmail.com>
2024-01-27 15:48:00 +0530

    balance-cashflow added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Header from "./components/Header"
+import Balance from "./components/Balance"
+import CashFlow from './components/CashFlow';
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance />
+        <CashFlow />
+      </div>
+      
     </div>
   );
 }
