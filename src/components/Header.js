4f7d8e99c9ad0a406102941e5aeb0fd67d77af07
Abhishek <abhisoni1764@gmail.com>
2024-01-27 15:35:17 +0530

    header-added

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <div className="header">
+            <h1 className="header-text">Expense Tracker</h1>
+        </div>
+    )
+    
+}
